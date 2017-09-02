6900676748d3e000f5ef3649070f335dfaab92eb
Jörg Ramb <gitlab@jramb.com>
2017-09-02 17:31:28 +0200

    just some changes, still wip

--- src/net/nogui/stacker.clj
+++ src/net/nogui/stacker.clj
@@ -6,7 +6,7 @@
             [instaparse.core :as instaparse]
             [clojure.pprint :as pp])
   (:import
-    (java.util LinkedList)
+    ;;(java.util LinkedList)
     (jline.console ConsoleReader)
     (jline.console.completer AnsiStringsCompleter)
     (jline.console.completer CandidateListCompletionHandler)
@@ -18,31 +18,55 @@
 
 (def prompt "\u001B[32m>\u001B[0m ")
 
-(defn func2 [f]
-  (fn [s]
-    (let [a (first s)
-          b (second s)
-          r (rest (rest s))]
-      (conj r (f b a)))))
+(defn spop [s]
+  [(pop s) (peek s)])
+
+(defn func2
+  "Takes a binary function and returns a stacker-function which expects two
+  items on the stack, performs the function on it and pushes the result back on
+  the stack. In other words: it makes a ( a b -- (f b a) )"
+  [f]
+  (fn [s env]
+    (let [[s a] (spop s)
+          [s b] (spop s)]
+      [(conj s (f b a)) env])))
+
+(defn env-neutral-function
+  "Takes a function (s->s) and returns a stacker-function which does not change the env."
+  [f]
+  (fn [s env]
+    [(f s) env]))
 
 (defn top-if [stack cond else]
-  (let [t (first stack)]
+  (let [t (peek stack)]
     (if (cond t) t else)))
 
+
+(defn benchmark []
+  (let [s (range 10000)]
+    ()))
+
+
+;; All stacker functions take a stack and an env and return a stack and an env
 (def default-env
-  (atom 
-  {"." (fn [s]
-          (println (first s))
-          (rest s))
-   "clear"  (fn [s] ())
-   "bye"    (fn [s] (System/exit (top-if s number? 0)))
-   "dup"    (fn [s] (conj s (first s)))
-   "drop"   (fn [s] (rest s))
-   "swap"   (fn [s] (let [[a b & r] s] (conj (conj r a) b)))
+  (atom
+  {"." (fn [s env]
+         (println (peek s))
+         [(pop s) env])
    "+"      (func2 +)
    "-"      (func2 -)
    "*"      (func2 *)
-   "/"      (func2 /)}))
+   "/"      (func2 /)
+   "p" (fn [s env]
+         (println (peek s))
+         [s env])
+   "clear"  (fn [s env] [() env])
+   "drop"   (fn [s env] [(pop s) env])
+   "dup"    (fn [s env] [(conj s (peek s)) env])
+   "env"    (fn [s env] (println (sort (keys env)))
+              [s env])
+   "q"      (fn [s env] (System/exit (top-if s number? 0)))
+   "swap"   (fn [s env] (let [[a b & r] s] (conj (conj r a) b)))}))
 
 (defn make-line-reader []
   (let [cr (ConsoleReader.)]
@@ -54,15 +78,16 @@
 (defn print-stack [stack]
   (println "S: " (str/join " " (reverse stack))))
 
-(def parser (instaparse/parser
-              "S = (blank|word|str|num)* 
-              <blank> = <#'\\s+'>
-              num = #'-?[0-9]+\\.?[0-9]*'
-              str = <'\"'> #'[^\"]*' <'\"'>
-              word = #'[^-0-9\\s\"][^\\s]*' "
-              ))
+(def parser
+  (instaparse/parser
+   "S = (blank|word|str|num)*
+    <blank> = <#'\\s+'>
+    num = #'-?[0-9]+\\.?[0-9]*'
+    str = <'\"'> #'[^\"]*' <'\"'>
+    word = #'[^0-9\\s\"][^\\s]*'
+"))
 
-(defn apply-tokens [stack env tokens]
+(defn apply-tokens [[stack env] tokens]
   ;(pp/pprint tokens)
   ;(pp/pprint stack)
   (if (empty? tokens)
@@ -73,11 +98,13 @@
       (condp = kind
         :word (let [f (get env value)]
                 (if (nil? f)
-                  (println "*** Word not defined:" value)
-                  (recur (f stack) env remaining)))
+                  (do
+                    (println "*** Word not defined:" value "(Aborting execution)")
+                    [stack env])
+                  (recur (f stack env) remaining)))
         :num (let [v (read-string value)]
-               (recur (conj stack v) env remaining))
-        :str (recur (conj stack value) env remaining)))))
+               (recur [(conj stack v) env] remaining))
+        :str (recur [(conj stack value) env] remaining)))))
 
 ;(pp/pprint (parser "dup 3.4 3.4 3.4 dip \"hwody bowdy\" and"))
 
@@ -93,14 +120,14 @@
   (let [lr (make-line-reader)
         env @default-env
         start-tokens (string-to-tokens (str/join " " start-words) env lr)
-        initial (apply-tokens () env start-tokens)]
+        initial (apply-tokens [() env] start-tokens)]
     (loop [[stack env] initial]
       (print-stack stack)
       (let [r (.readLine lr prompt)
             e (string-to-tokens r env lr)]
         (if (or (not e) (= r "bye"))
           stack
-          (recur (apply-tokens stack env e)))))))
+          (recur (apply-tokens [stack env] e)))))))
 
 
 (defn -main [& args]
